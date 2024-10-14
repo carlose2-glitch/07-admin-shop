396ad6d6b8ce1448cd7253b7d1c28e5a6a994ff8
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-10-14 14:55:39 -0400

    obteniendo las imagenes

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- component -->
+  <!-- Create By Joker Banny -->
+  <!-- Header Navbar -->
+  <top-menu />
+  <!-- Title -->
+
+  <!-- RouterView -->
+  <router-view />
+
+  <custom-footer />
+  <!-- Footer -->
+</template>
+
+<script lang="ts" setup>
+import TopMenu from '../components/TopMenu.vue';
+import CustomFooter from '../components/CustomFooter.vue';
+
+TopMenu;
+</script>
