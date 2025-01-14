f5b5949e77684043c1575d5acf646afcea546a13
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2025-01-14 16:14:52 -0400

    diseño de la tienda

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- Header Navbar -->
+  <TopMenu />
+
+  <RouterView />
+
+  <!-- Footer -->
+  <CustomFooter />
+</template>
+
+<script lang="ts" setup>
+import TopMenu from '../components/TopMenu.vue';
+import CustomFooter from '../components/CustomFooter.vue';
+</script>
