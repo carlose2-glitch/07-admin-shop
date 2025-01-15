3b90d22396af8fce4917ad770c6a9cf3d4ce8cb8
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2025-01-15 15:50:50 -0400

    obtener productos paginados en la consola

--- src/modules/shop/views/HomeView.vue
+++ src/modules/shop/views/HomeView.vue
@@ -590,7 +590,7 @@
 </template>
 
 <script lang="ts" setup>
-import { tesloApi } from '@/api/tesloApi';
+import { getProductsAction } from '@/modules/products/actions';
 
-tesloApi;
+getProductsAction();
 </script>
