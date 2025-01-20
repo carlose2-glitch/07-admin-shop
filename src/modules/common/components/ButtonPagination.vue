3382d9e1b553664ca8208ae285e08195d1056758
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2025-01-20 12:43:45 -0400

    mostrar las paginas de los productos de la tienda

---
+++ src/modules/common/components/ButtonPagination.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex space-x-3 justify-center py-10 bg-gray-100">
+    <button
+      :disabled="page === 1"
+      @click="$router.push({ query: { page: page - 1 } })"
+      class="flex items-center space-x-1.5 bg-blue-500 rounded-lg px-4 py-1.5 disabled:bg-gray-300 text-white hover:bg-blue-600 transition-all"
+    >
+      <ArrowLeft />
+      <span>Anteriores</span>
+    </button>
+
+    <button
+      :disabled="hasMoreData"
+      @click="$router.push({ query: { page: page + 1 } })"
+      class="flex items-center space-x-1.5 bg-blue-500 rounded-lg px-4 py-1.5 disabled:bg-gray-300 text-white hover:bg-blue-600 transition-all"
+    >
+      <span>Siguientes</span>
+      <ArrowRight />
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ArrowLeft from '@/icons/ArrowLeft.vue';
+import ArrowRight from '@/icons/ArrowRight.vue';
+
+interface Props {
+  page: number;
+
+  hasMoreData: boolean;
+}
+
+defineProps<Props>();
+</script>
